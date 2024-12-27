d3fab7e4376d958862a2480ff24ba09d357a428a
Azil <developmentwithazil@gmail.com>
2024-12-27 19:41:08 +0500

    First Commit

---
+++ react/src/router.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Dashboard from "./view/Dashboard";
+import Survey from "./view/Survey";
+import Login from "./view/Login";
+import Signup from "./view/Signup";
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
+
+const router = createBrowserRouter ([
+   {
+    path: '/',
+    element: <DefaultLayout />,
+    children: [
+        {
+            path: '/dashboard',
+            element: <Navigate to='/' />
+        },
+        {
+            path: '/',
+            element: <Dashboard />
+        },
+        {
+            path: '/survey',
+            element: <Survey />
+        },
+    ]
+   },
+   {
+    path: '/',
+    element: <GuestLayout />,
+    children: [
+        {
+            path: 'login',
+            element: <Login />
+        },
+        {
+            path: 'signup',
+            element: <Signup />
+        },
+    ]
+   }
+])
+
+export default router;
